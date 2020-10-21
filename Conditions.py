c9b330c051dbc8248360fc52bf36faf70329c203
unknown <tomassz.boberek@gmail.com>
2020-10-21 22:26:55 +0100

    Basic Condition checking implemented

--- Conditions.py
+++ Conditions.py
@@ -18,3 +18,8 @@ class Condition:
 
     def get_treatment(self):
         return self.treatment
+
+Dead = Condition("Mortem", "The Patient is dead mate, they ain't coming back from this one", ['Patient has no pulse', 'Patient is not breathing', 'patient is non-responsive'], "Nothing short of a miracle will bring them back from this one")
+Bursitis = Condition("Bursitis", "Joints become painful, tender and swollen that can be treated at home", "Joint is painful, tender or swollen", "Treatment involves Resting, use of Ice on the joint and elevating the affected limb")
+Absess = Condition("Absess", "A swollen, pus-filled lump under the surface of the skin caused by a bacterial infection.", ['Pain in affected area','high temperature', 'generally feeling unwell'],"May drain naturally with time. Antibiotics Treatment with incision to drain the pus")
+Pancreatitis = Condition("Acute Pancreatitis", "Acute pancreatitis is a condition where the pancreas becomes inflamed over a short period of time", "Severe Pain in the abdomen, feeling sick, diarrhoea, fever", "Hospital admission over several days, Intravenous fluids, and Pain relief")
--- Medsys.py
+++ Medsys.py
@@ -1,8 +1,16 @@
 from art import *
-from Conditions import Condition
+from Conditions import *
 
-Bursitis = Condition("Bursitis", "Joints become painful, tender and swollen that can be treated at home", "Joint is painful, tender or swollen", "Treatment involves Resting, use of Ice on the joint and elevating the affected limb")
-Absess = Condition("Absess", "A swollen, pus-filled lump under the surface of the skin caused by a bacterial infection.", ['Pain in affected area','high temperature', 'generally feeling unwell'],"May drain naturally with time. Antibiotics Treatment with incision to drain the pus")
-Pancreatitis = Condition("Acute Pancreatitis", "Acute pancreatitis is a condition where the pancreas becomes inflamed over a short period of time", "Severe Pain in the abdomen, feeling sick, diarrhoea, fever", "Hospital admission over several days, Intravenous fluids, and Pain relief")
-Dead = Condition("Mortem", "")
-print(*Absess.get_symptoms(), sep = "\n")
+
+
+def main():
+
+    #Checking for user input
+    usercondition = input("Enter a condition:\n")
+    
+    if usercondition in globals():
+        print("true")
+    else:
+        print("false")
+
+main()
